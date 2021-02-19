ccba9d4a4ffa5df0c6edb7f3811627bfea61a646
Prabhakar Acharya <acharyaP03@gmail.com>
2021-02-19 18:38:24 +1100

    Keeping App.vue lean

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,47 @@
-
 <template>
-  <ul>
-    <learning-resources
-      v-for="resources in storedResources"
-      :key="resources.id"
-      :title="resources.title"
-      :description="resources.description"
-      :link="resources.link"
-    ></learning-resources>
-  </ul>
-  <!-- Compiler warning  -->
+  <stored-resources :stored-resources="storedResources"></stored-resources>
 </template>
 
 <script>
-import LearningResources from './components/LearningResources/LearningResources.vue';
+import StoredResources from "./components/LearningResources/StoredResources.vue";
+
 export default {
+    
   components: {
-    LearningResources
+    StoredResources,
   },
   data() {
     return {
       storedResources: [
         {
-          id: 'official-guide',
-          title: 'Offical Guide',
-          description: 'The official Vue.js documentation',
-          link: 'https://vuejs.org'
+          id: "official-guide",
+          title: "Offical Guide",
+          description: "The official Vue.js documentation",
+          link: "https://vuejs.org",
         },
         {
-          id: 'official-guide',
-          title: 'Official guide for React',
-          description: 'The official React.js documentation',
-          link: 'https://react.js'
-        }
-      ]
+          id: "official-guide",
+          title: "Official guide for React",
+          description: "The official React.js documentation",
+          link: "https://react.js",
+        },
+      ],
     };
-  }
+  },
 };
 </script>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/LearningResources/StoredResources.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <learning-resources
+      v-for="resources in storedResources"
+      :key="resources.id"
+      :title="resources.title"
+      :description="resources.description"
+      :link="resources.link"
+    ></learning-resources>
+  </ul>
+</template>
+
+<script>
+import LearningResources from "./LearningResources.vue";
+export default {
+  components: {
+    LearningResources,
+  },
+  props: {
+    storedResources: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}</style>
