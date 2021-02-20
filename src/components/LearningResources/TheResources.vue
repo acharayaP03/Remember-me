f76bc045b7411b1660aad88b84f52f2fd9873f63
Prabhakar Acharya <acharyaP03@gmail.com>
2021-02-20 21:18:11 +1100

    Moving all logic to computed props

--- src/components/LearningResources/TheResources.vue
+++ src/components/LearningResources/TheResources.vue
@@ -1,15 +1,11 @@
 <template>
   <div>
     <base-card>
-      <base-button
-        @click="setSelectedComponent('add-resources')"
-        :mode="selectedComponenet === 'add-resources' ? null : 'flat'"
-        >Add Resources</base-button
-      >
-      <base-button
-        @click="setSelectedComponent('stored-resources')"
-        :mode="selectedComponenet === 'stored-resources' ? null : 'flat'"
+      <base-button @click="setSelectedComponent('stored-resources')" :mode="storedResourcesMode"
         >Stored Resources</base-button
+      >
+      <base-button @click="setSelectedComponent('add-resources')" :mode="addResourcesMode"
+        >Add Resources</base-button
       >
     </base-card>
     <component :is="selectedComponenet"></component>
@@ -43,13 +39,21 @@ export default {
       ],
     };
   },
+  computed: {
+    storedResourcesMode() {
+      return this.selectedComponenet === "stored-resources" ? null : "flat";
+    },
+    addResourcesMode() {
+      return this.selectedComponenet === "add-resources" ? null : "flat";
+    },
+  },
   provide() {
     return {
       resources: this.storedResources,
     };
   },
   methods: {
-    setSelectedResources(element) {
+    setSelectedComponent(element) {
       this.selectedComponenet = element;
     },
   },
